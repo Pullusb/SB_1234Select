3932dea545544e290672b13eb81c185b57553920
Pullusb <bernou.samuel@gmail.com>
2015-08-03 19:42:35 +0200

    first

---
+++ README.md
@@ -0,0 +1,31 @@
+# SB 1234Select
+
+Add shortcut for switching swiftly between select modes in 3D view and UVeditor
+
+--------
+
+Why ?
+
+To switch between selection mode in Blender you have to do "ctrl+tab then key" or "move mouse + clic + reposition mouse". These actions are heavily repeated during modelling and deserves dedicated keys. Guess what ? these are available !
+
+Description:
+
+This addon map hotkeys for edit mode only on the top row numbers keys.
+(incompatible with "emulate numpad" option)
+
+3Dview:
+1 - Vertex 
+2 - Edge 
+3 - Face
+4 - Limit selection to visible (occlude geometry)
+
+UVeditor:
+1 - Vertex
+2 - Edge
+3 - Face
+4 - Island
+
+Now you don't have to hit ctrl+tab before ! Just hit one key and speed-up your workflow.
+
+Thanks to CoDEmanX and Atom for their helps (intended or not ^^)
+Happy blending
---
+++ SB_1234Select.py
@@ -0,0 +1,80 @@
+# ##### BEGIN GPL LICENSE BLOCK #####
+#
+#  This program is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License
+#  as published by the Free Software Foundation; either version 2
+#  of the License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software Foundation,
+#  Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+#
+# ##### END GPL LICENSE BLOCK #####
+
+bl_info = {
+    "name": "1234Select",
+    "author": "Samuel Bernou",
+    "version": (1, 1),
+    "blender": (2, 75, 0),
+    "location": "View3D/UVeditor(editmode) > 1,2,3,4 keys",
+    "description": "Quick switch for selection mode ",
+    "warning": "Incompatible with emulate numpad",
+    "wiki_url": "",
+    "tracker_url": "",
+    "category": "3D View"}
+
+import bpy
+
+mesh_sel_keymap = {
+    'ONE': "True, False, False",
+    'TWO': "False, True, False",
+    'THREE': "False, False, True"
+}
+
+uv_sel_keymap = {
+    'ONE': "VERTEX",
+    'TWO': "EDGE",
+    'THREE': "FACE",
+    'FOUR': "ISLAND"
+}
+
+quickSelect_keymaps = [] # list of new custom keymap
+
+def register():
+    wm = bpy.context.window_manager
+
+    # keymap set for view3D only in editmode
+    km = wm.keyconfigs.default.keymaps['Mesh']
+    for k, v in mesh_sel_keymap.items():
+        kmi = km.keymap_items.new('wm.context_set_value', k, 'PRESS')
+        kmi.properties.data_path = 'tool_settings.mesh_select_mode'
+        kmi.properties.value = v
+        quickSelect_keymaps.append((km,kmi))
+        
+    kmi = km.keymap_items.new('wm.context_toggle', 'FOUR', 'PRESS')
+    kmi.properties.data_path = 'space_data.use_occlude_geometry'
+    quickSelect_keymaps.append((km,kmi))
+    
+    # keymap set for UVeditor
+    km = wm.keyconfigs.default.keymaps['UV Editor']
+    for k, v in uv_sel_keymap.items():
+        kmi = km.keymap_items.new('wm.context_set_enum', k, 'PRESS')
+        kmi.properties.data_path = 'tool_settings.uv_select_mode'
+        kmi.properties.value = v
+        quickSelect_keymaps.append((km,kmi))
+
+def unregister():
+    wm = bpy.context.window_manager
+    # remove the keymap
+    for km, kmi in quickSelect_keymaps:
+        km.keymap_items.remove(kmi)
+    # clear the list
+    quickSelect_keymaps.clear()
+    
+if __name__ == "__main__":
+    register()
